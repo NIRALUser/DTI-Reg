30240ee2dbc0f0daf2f6bb58b86634818d5a875c
Hans Johnson <hans-johnson@uiowa.edu>
2014-07-04 22:57:23 +0000

    COMP:  find_package ordering seems to be important
    When find_package(SlicerExecutionModel) follows
    find_pack(ITK), then for some reason the ITK_INCLUDE_DIRS
    is very truncated.

--- DTIReg.cmake
+++ DTIReg.cmake
@@ -1,16 +1,5 @@
-
-
 set(MODULE_NAME ${EXTENSION_NAME}) # Do not use 'project()'
 set(MODULE_TITLE ${MODULE_NAME})
-
-#-----------------------------------------------------------------------------
-find_package(SlicerExecutionModel NO_MODULE REQUIRED GenerateCLP)
-include(${GenerateCLP_USE_FILE})
-include(${SlicerExecutionModel_USE_FILE})
-
-include(${CMAKE_CURRENT_LIST_DIR}/Common.cmake)
-
-SETIFEMPTY(INSTALL_RUNTIME_DESTINATION bin)
 
 #-----------------------------------------------------------------------------
 # Update CMake module path
@@ -36,8 +25,17 @@ endif()
 include(${ITK_USE_FILE})
 
 #-----------------------------------------------------------------------------
+find_package(SlicerExecutionModel NO_MODULE REQUIRED GenerateCLP)
+include(${GenerateCLP_USE_FILE})
+include(${SlicerExecutionModel_USE_FILE})
+
+
+#-----------------------------------------------------------------------------
 find_package(BatchMake REQUIRED)
 include(${BatchMake_USE_FILE})
+
+include(${CMAKE_CURRENT_LIST_DIR}/Common.cmake)
+SETIFEMPTY(INSTALL_RUNTIME_DESTINATION bin)
 
 configure_file( "${CMAKE_CURRENT_SOURCE_DIR}/DTI-Reg_Config.h.in"
                 "${CMAKE_CURRENT_BINARY_DIR}/DTI-Reg_Config.h")
